a412fce8fcac0a8d0b541ae7c45963a9e05528f3
Antoine Ctrs <50273074+antoinectrs@users.noreply.github.com>
2020-12-01 14:07:47 +0100

    Snap BUG

---
+++ pipes_workspace/pipes_workspace/js/App.js
@@ -0,0 +1,95 @@
+console.log("hello, pipes begin");
+// constructor() {
+// this.canvas = document.createElement("canvas");
+// document.body.appendChild(this.canvas);
+//ctxx
+// this.ctx = this.canvas.createCanvas(200, 200);
+// this.canvas.width = this.w = window.innerWidth;
+// this.canvas.height = this.h = window.innerHeight;
+// this.setup();
+// }
+
+//PIPE GLOBAL VALUE
+let pipe;
+let shape1;
+let pipeNumber = 1;
+let pipeElement;
+
+//GRID GLOBAL VALUE
+let gridSpace = 100;
+let gridOffset = gridSpace / 2;
+
+//SET UP
+function setup() {
+  let width = windowWidth;
+  let height = windowHeight;
+  var canvas = createCanvas(width, height);
+
+  // canvas.parent('canvasForHTML');
+  // this.button = new Button(this.w / 2, this.h / 2, 50, 50, this.ctx);
+  //FB
+  //listener
+  // DATABASE.ref("COLOR_CHANGE").on("value", (snapshot) => {
+  //   if (!this.appHasStarted) {
+  //     this.button.changeColor(snapshot.val());
+  //     this.draw();
+  //     this.appHasStarted = true;
+  //   } else {
+  //     const value = snapshot.val();
+  //     this.button.changeColor(value);
+  //   }
+  // });
+
+  // this.draw();
+  //new grid class
+  grid = new Grid(gridSpace);
+  pipe = new Pipe(windowWidth / 2, windowHeight / 2, 100, 300);
+  // pipe.push(pipeElement);
+  shape1 = new Draggable(200, 100, 50, 150);
+}
+
+function draw() {
+  background(255, 20);
+
+  //show grid class
+  strokeWeight(2);
+  stroke(0);
+  grid.showG();
+
+  //loop draw pipe
+  // for (let index = 0; index < pipeNumber; index++) {
+  //   pipe[index].update();
+  //   pipe[index].show();
+  //   // console.log(pipeNumber)
+  // }
+  pipe.update();
+  pipe.show();
+  //square swipe test
+  // shape1.update();
+  // shape1.show();
+
+  // this.button.show();
+}
+
+function mousePressed() {
+  // for (let index = 0; index < pipeNumber; index++) {
+  //   pipe[index].pressed(gridSpace, gridOffset);
+  // }
+  pipe.pressed();
+  shape1.pressed();
+  
+}
+
+function mouseReleased() {
+  // for (let index = 0; index < pipeNumber; index++) {
+  //   pipe[index].released();
+  // }
+  let x=  pipe.snap(mouseX,gridSpace, gridOffset);
+  let y=  pipe.snap(mouseY,gridSpace, gridOffset);
+  pipe.released(x,y);
+  
+  shape1.released();
+ // pipeElement = new Pipe(windowWidth / 2, windowHeight / 2, 100, 300);
+  // pipe.push(pipeElement);
+  pipeNumber++;
+}
---
+++ pipes_workspace/pipes_workspace/js/pipe.js
@@ -0,0 +1,83 @@
+// let images = [];
+let svg_01;
+let imageNumber = 1;
+function preload() {
+  for (let i = 0; i < imageNumber; i++) {
+    svg_01 = loadImage("pipes_folder/pipes_01.svg");
+  }
+}
+
+class Pipe {
+  constructor(x,y,w,h) {
+    this.dragging = false; // Is the object being dragged?
+    this.rollover = false; // Is the mouse over the ellipse?
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.image;
+    this.src = svg_01;
+
+    //SNAP
+    this.px;
+    this.py;
+  }
+
+  update() {
+    // Adjust location if being dragged
+    if (this.dragging) {
+      this.x = mouseX + this.offsetX;
+      this.y = mouseY + this.offsetY;
+    }
+  }
+  show() {
+    imageMode(CORNER);
+    this.image = image(this.src, this.x, this.y,this.w,this.h);
+  }
+
+  pressed(gridSpace, gridOffset) {
+      //slide in rectangle zone
+      //** BUG SIZE **
+    if (
+      mouseX > this.x &&
+      mouseX < this.x + this.w &&
+      mouseY > this.y &&
+      mouseY < this.y + this.h
+    ) {
+      this.dragging = true;
+      // If so, keep track of relative location of click to corner of rectangle
+      this.offsetX = this.x - mouseX;
+      this.offsetY = this.y - mouseY;
+    }
+    
+    //BEGIN TO SNAP
+    // this.x = snap(mouseX,gridSpace, gridOffset);
+    // this.y = snap(mouseY,gridSpace, gridOffset);
+    // this.px = snap(pmouseX);
+    // this.py = snap(pmouseY,gridSpace, gridOffset);
+  }
+
+  released(x,y) {
+    this.x = x;
+    this.y = y;
+    // Quit dragging
+    this.dragging = false;
+  }
+
+  snap(op,gridSpace, gridOffset) {
+    
+    // subtract offset (to center lines)
+    // divide by grid to get row/column
+    // round to snap to the closest one
+    this.cell = Math.round((op - gridOffset) / gridSpace);
+    // multiply back to grid scale
+    // add offset to center
+    console.log(this.cell)
+
+    return this.cell * gridSpace + gridOffset;
+
+  }
+}
